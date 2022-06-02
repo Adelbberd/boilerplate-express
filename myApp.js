dbc1ffee3a33d95236ce1e0a4c701d9795cfb93d
Adelberd Mothusi Motlhabinyana <68590967+Adelbberd@users.noreply.github.com>
2022-06-02 13:07:47 +0000

    environment variables

--- myApp.js
+++ myApp.js
@@ -8,11 +8,24 @@ console.log('Hello World');
 // });
 
 // Serve an HTML file.
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html')
-});
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/views/index.html')
+// });
 
 // Serve static assets with middleware.
-app.use('/public',express.static(__dirname + '/public'));
+// app.use('/public',express.static(__dirname + '/public'));
+
+//Serve a json object.
+
+
+app.get('/json', function(req, res){
+const mySecret = process.env['MESSAGE_STYLE'];
+if(mySecret == 'uppercase') {
+  res.json({message: "HELLO JSON"});
+  
+} else {
+  res.json({message: "Hello json"});
+}
+});
 
 module.exports = app;
