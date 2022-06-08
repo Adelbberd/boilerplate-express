de70dd7a39de83b1185053a99a4625d2a89aaa49
Adelberd Mothusi Motlhabinyana <68590967+Adelbberd@users.noreply.github.com>
2022-06-08 07:33:11 +0000

    chain middleware to create time server

--- myApp.js
+++ myApp.js
@@ -34,4 +34,13 @@ if(mySecret == 'uppercase') {
 }
 });
 
+// Chain Middleware to create time server.
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString();
+  next();
+},
+  function(req, res){
+  res.send({time : req.time});
+})
+
 module.exports = app;
